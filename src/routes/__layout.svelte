2018611c25965284c75d7a603e4c2e31f1ed3c51
Chris Ellis <ellis.cs@gmail.com>
2021-08-12 11:45:11 +0100

    Update Header and layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.postcss';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<div class="bg-gray-200 h-full w-full min-h-screen">
+	<div class="w-full max-w-screen-xl mx-auto">
+		<Header />
+		<main class="px-6">
+			<slot />
+		</main>
+		<Footer />
+	</div>
+</div>
